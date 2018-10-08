dbce3b033151ca9eb5cb491ce8ca310d7f8c357c
Foenki <pierrot.lagier@wanadoo.fr>
2018-10-08 20:35:56 +0200

    Improve concurrent solver

--- src/Visualization2D/Visualization2D.java
+++ src/Visualization2D/Visualization2D.java
@@ -6,15 +6,16 @@ public class Visualization2D
 {
     public static void main(String[] args)
     {
-        Solitaire solitaire = new Solitaire("data/boards/classique1.sol");
+        Solitaire solitaire = new Solitaire("data/boards/board1_concours.sol");
         try
         {
             Fenetre fenetre = new Fenetre(solitaire);
 
             SolverLargeur solverLargeur = new SolverLargeur(10, 50);
             SolverIterations solver = new SolverIterations(solverLargeur, 1);
-            SolverConcurrent solverConcurrent = new SolverConcurrent(solver, 3);
+            SolverConcurrent solverConcurrent = new SolverConcurrent(solver, 8);
             Chemin chemin = solverConcurrent.solve(solitaire);
+            System.out.println("Meilleur score : " + solverConcurrent.getMeilleurScore());
 
             for(Coup coup : chemin.getCoups())
             {
